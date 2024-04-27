ab73bdb1da85ddb014f23f9f7b95c1bda917223e
nabrain <michal2002.60@gmail.com>
2024-04-27 18:57:19 +0200

    Add Book class for instantiating book objects

---
+++ src/main/java/app/bookmanagement/domain/Book.java
@@ -0,0 +1,78 @@
+package app.bookmanagement.domain;
+
+import java.util.Objects;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private String genre;
+    private int year;
+    private int ID;
+
+    public Book(String title, String author, String genre, int year) {
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book book)) return false;
+        return year == book.year && ID == book.ID && Objects.equals(title, book.title) && Objects.equals(author, book.author) && Objects.equals(genre, book.genre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, genre, year, ID);
+    }
+
+    public String toString() {
+        return "Title: " + this.title +
+                " Author: " + this.author +
+                " Genre: " + this.genre +
+                " Year: " + this.year;
+    }
+}
