27dc2fa87f4eb6fda54dd6c5fc2af69f4e6c6c3d
nabrain <michal2002.60@gmail.com>
2024-04-27 19:52:18 +0200

    fix DAO interface

--- src/main/java/app/bookmanagement/domain/DAO.java
+++ src/main/java/app/bookmanagement/domain/DAO.java
@@ -1,4 +1,13 @@
 package app.bookmanagement.domain;
 
-public class DAO {
+import java.sql.SQLException;
+import java.util.List;
+
+
+public interface DAO<T> {
+    T findById(Long id) throws SQLException;
+    List<T> findAll() throws SQLException;
+    int add(T t) throws SQLException;
+    void update(T t) throws SQLException;
+    void delete(Long id) throws SQLException;
 }
