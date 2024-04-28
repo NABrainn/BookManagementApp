482ff2c349991102e035abad94bde4cfe9168274
nabrain <michal2002.60@gmail.com>
2024-04-28 14:31:22 +0200

    fix .css connection line to prevent nullptr exception

--- src/main/java/app/bookmanagement/Main.java
+++ src/main/java/app/bookmanagement/Main.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.Objects;
 
 
 public class Main extends Application {
@@ -15,7 +16,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("BookManagement.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
-        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
         stage.setTitle("Book Management");
         stage.setScene(scene);
         stage.show();
